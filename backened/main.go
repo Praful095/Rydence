62f6232e3cbeacc6e851dfd851a0f9faef7e6746
Praful095 <prafful095@gmail.com>
2026-03-24 14:58:22 +0530

    fixed port for render

--- backened/main.go
+++ backened/main.go
@@ -1,33 +1,34 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"rydence/handlers"
-	"rydence/middleware"
+// import (
+// 	"fmt"
+// 	"rydence/handlers"
+// 	"rydence/middleware"
 
-	"github.com/gin-gonic/gin"
-)
+// 	"github.com/gin-gonic/gin"
+// )
 
-func main() {
-	// Gin router banao
-	r := gin.Default()
+// func main() {
+// 	// Gin router banao
+// 	r := gin.Default()
 
-	// CORS middleware lagao
-	// Bina iske React frontend backend se baat nahi kar payega
-	r.Use(middleware.CORS())
+// 	// CORS middleware lagao
+// 	// Bina iske React frontend backend se baat nahi kar payega
+// 	r.Use(middleware.CORS())
 
-	// All routes /api/v1 ke under
-	v1 := r.Group("/api/v1")
-	{
-		v1.GET("/pricing", handlers.GetPricing)
-		v1.GET("/predict", handlers.GetPredictionHandler)
-		v1.GET("/trend", handlers.GetTrendHandler)
+// 	// All routes /api/v1 ke under
+// 	v1 := r.Group("/api/v1")
+// 	{
+// 		v1.GET("/pricing", handlers.GetPricing)
+// 		v1.GET("/predict", handlers.GetPredictionHandler)
+// 		v1.GET("/trend", handlers.GetTrendHandler)
 
-		// Main endpoint - frontend sirf yahi use karega
-		v1.GET("/ride-info", handlers.GetFullRideInfo)
-	}
+// 		// Main endpoint - frontend sirf yahi use karega
+// 		v1.GET("/ride-info", handlers.GetFullRideInfo)
+// 	}
 
-	// Port 8080 pe server start karo
-	r.Run(":8080")
-	fmt.Println("Server running on http://localhost:8080")
-}
+// 	// Port 8080 pe server start karo
+// 	r.Run(":8080")
+// 	fmt.Println("Server running on http://localhost:8080")
+// }
+
