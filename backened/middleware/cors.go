cfc0264802fe7a9f367f83dbe5adb8bd15b71985
Praful095 <prafful095@gmail.com>
2026-03-24 17:26:38 +0530

    api issues bug second solvedd

--- backened/middleware/cors.go
+++ backened/middleware/cors.go
@@ -6,7 +6,7 @@ import "github.com/gin-gonic/gin"
 // Go backend (localhost:8080) pe requests allow karna
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "")
+		c.Header("Access-Control-Allow-Origin", "https://rydence-app.vercel.app")
 		c.Header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		c.Header("Access-Control-Allow-Headers", "Content-Type")
 
